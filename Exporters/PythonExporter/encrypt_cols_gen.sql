dba4fb953834fb971ea4fe488d1842396db13938
Lav Patel <contact.lavpatel@gmail.com>
2021-05-30 15:27:47 -0500

    inserting generated sql into a table

--- Exporters/PythonExporter/encrypt_cols_gen.sql
+++ Exporters/PythonExporter/encrypt_cols_gen.sql
@@ -1,41 +1,67 @@
+WHENEVER SQLERROR CONTINUE;
+drop table n3c_gen_sql;
+drop table n3c_cdm_tables;
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+
+------------------------------------------------------------------------------------------------------------------------------
+--- CDM tables to include
+------------------------------------------------------------------------------------------------------------------------------
+create table n3c_cdm_tables(
+    table_name varchar(255)
+);
+
+--Insert into N3C_CDM_TABLES (TABLE_NAME) values ('CDM_STATUS');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('CONDITION');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('DEATH');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('DEATH_CAUSE');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('DEMOGRAPHIC');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('DIAGNOSIS');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('DISPENSING');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('ENCOUNTER');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('ENROLLMENT');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('HARVEST');
+--Insert into N3C_CDM_TABLES (TABLE_NAME) values ('HASH_TOKEN');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('IMMUNIZATION');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('LAB_RESULT_CM');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('LDS_ADDRESS_HISTORY');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('MED_ADMIN');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('OBS_CLIN');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('OBS_GEN');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('PCORNET_TRIAL');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('PRESCRIBING');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('PROCEDURES');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('PROVIDER');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('PRO_CM');
+Insert into N3C_CDM_TABLES (TABLE_NAME) values ('VITAL');
+
+------------------------------------------------------------------------------------------------------------------------------
+--- Save gen sql in table
+------------------------------------------------------------------------------------------------------------------------------
+create table n3c_gen_sql(
+    sql_str varchar(4000)
+);
+
+insert into n3c_gen_sql (select 'define encrypting_password=&1;' from dual);
 ------------------------------------------------------------------------------------------------------------------------------
 --- DROP TABLE SQL
------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------
+insert into n3c_gen_sql (select 'WHENEVER SQLERROR CONTINUE ;' from dual);
+
+insert into n3c_gen_sql
 with cdm_tables_w_patid
 as
 (
 select distinct table_name
 from dba_tab_cols
 where owner = 'PCORNET_CDM'
-    and table_name in (
-                        'CDM_STATUS'
-                        ,'CONDITION'
-                        ,'DEATH'
-                        ,'DEATH_CAUSE'
-                        ,'DEMOGRAPHIC'
-                        ,'DIAGNOSIS'
-                        ,'DISPENSING'
-                        ,'ENCOUNTER'
-                        ,'ENROLLMENT'
-                        ,'HARVEST'
-                        ,'HASH_TOKEN'
-                        ,'IMMUNIZATION'
-                        ,'LAB_RESULT_CM'
-                        ,'LDS_ADDRESS_HISTORY'
-                        ,'MED_ADMIN'
-                        ,'OBS_CLIN'
-                        ,'OBS_GEN'
-                        ,'PCORNET_TRIAL'
-                        ,'PRESCRIBING'
-                        ,'PROCEDURES'
-                        ,'PROVIDER'
-                        ,'PRO_CM'
-                        ,'VITAL'
-                        )
+    and table_name in ( select table_name from N3C_CDM_TABLES)
 )
 select 'DROP TABLE PCORNET_CDM_N3C.'||table_name || ' purge ;' from cdm_tables_w_patid order by table_name
 ;
 
+insert into n3c_gen_sql (select 'WHENEVER SQLERROR EXIT SQL.SQLCODE;' from dual);
+
+commit;
 ------------------------------------------------------------------------------------------------------------------------------
 --- CREATE TABLE SQL
 -----------------------------------------------------------------------------------------------------------------------------
